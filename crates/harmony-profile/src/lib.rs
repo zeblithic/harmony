da23ad2f46d2cc5873dedcbd01e1ccd2456effb5
Jake Englund <zeblith@gmail.com>
2026-03-20 13:51:43 -0700

    feat(profile): add EndorsementRecord and EndorsementBuilder

--- crates/harmony-profile/src/lib.rs
+++ crates/harmony-profile/src/lib.rs
@@ -1,8 +1,10 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 extern crate alloc;
 
+pub mod endorsement;
 pub mod error;
 pub mod profile;
 
+pub use endorsement::{EndorsementBuilder, EndorsementRecord};
 pub use error::ProfileError;
 pub use profile::{ProfileBuilder, ProfileRecord};
